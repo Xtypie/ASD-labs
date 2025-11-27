35b7b2ba81a46aca0a82278108955f181ac23ef4
Xtypie <xtypie2505@gmail.com>
2025-11-27 13:31:17 +0300

    add: lab9, 15-16

---
+++ lab9.py
@@ -0,0 +1,33 @@
+def heapify(nums, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and nums[left] > nums[largest]:
+        largest = left
+
+    if right < n and nums[right] > nums[largest]:
+        largest = right
+
+    if largest != i:
+        nums[i], nums[largest] = nums[largest], nums[i]
+        heapify(nums, n, largest)
+
+
+def heap_sort(nums):
+    n = len(nums)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(nums, n, i)
+
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapify(nums, i, 0)
+
+    return nums
+
+
+nums = [0, 2, 3, 1, 5, 4, 8, 7, -1]
+
+print(heap_sort(nums))
+
